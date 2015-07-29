6d77daf846b0522bacd107e5009f26274e054cb7
master-vic <victor_neville@linux.com>
2015-07-28 20:21:27 -0400

    update

--- demo.py
+++ demo.py
@@ -13,7 +13,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Description: This code generates user forms after receiving and analysing
+Description: This code returns userform key words after receiving and analysing
              text data from file. The algorithms use natural language
              processing techniques.
 
