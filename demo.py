4c3a5e7bfc3c3aeda0040508095cc4f01f895651
master-vic <victor_neville@linux.com>
2015-08-01 15:23:22 -0400

    update

--- demo.py
+++ demo.py
@@ -12,8 +12,10 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Description: This code returns a userform name after analysing data from text file.
              The algorithms strategies use natural language processing techniques.
+
 File: demo.py
 Author: Victor Neville
 Python version: 3.4.0
@@ -33,65 +35,28 @@ from nltk.metrics import edit_distance as dist
 MAX_DIST = (2,)
 MIN_LENGTH = (3,)
 THRESHOLD = (0.95,)
-SEN_FILE = ("sentences.txt",)
-USER_FORM = (#userform name     extra words
-            ("dentist",         ()),
-            ("bodyguard",       ()),
-            ("storage",         ()),
-            ("loan",            ()),
-            ("dustman",         ("trash",)),
-            ("snow",            ("shovel",)),
-            ("caterer",         ("catrer",)),
-            ("mover",           ("moving",)),
-            ("ticket",          ("hockey",)),
-            ("therapist",       ("massage",)),
-            ("guide",           ("gide", "visit")),
-            ("renting",         ("rent", "rant")),
-            ("pool",            ("basin", "pol")),
-            ("trainer",         ("gym", "workout")),
-            ("exterminator",    ("roach", "insect")),
-            ("assembler",       ("set", "assemble")),
-            ("tattooist",       ("tattoo", "piercer")),
-            ("veterinary",      ("animal", "cat", "dog")),
-            ("delivery",        ("bier", "beer", "pizza")),
-            ("tutor",           ("tutoring", "teach", "learn")),
-            ("carpooling",      ("getting", "lift", "carpool")),
-            ("plumber",         ("toilet", "conditioner", "swing")),
-            ("babysitter",      ("kid", "children", "housekeeping")),
-            ("taxi",            ("driver", "drive", "pick", "ride")),
-            ("article",         ("shoe", "watch", "perfume", "dress")),
-            ("photographer",    ("picture", "photo", "pic", "photograf")),
-            ("programmer",      ("developer", "web", "computer", "network")),
-            ("couturier",       ("shirt", "fashion", "tailor", "clothe",
-                                 "t-shirt")),
-            ("gardener",        ("lawn", "flower", "garden", "plant",
-                                "gardn", "gardening", "mowed")),
-            ("housemaid",       ("waitress", "lady", "dish", "laundry",
-                                 "menage", "server")),
-            ("car",             ("garage", "truk", "towing", "carburator", 
-                                 "carburetor", "truck")),
-            ("event",           ("musician", "bouncer", "security", "DJ",
-                                 "sound", "mix", "song")),
-            ("booking",         ("schedule", "meeting", "appointment",
-                                 "book", "pm", "am")),
-            ("house",           ("residence", "porch", "deck", "roof", "dec",
-                                 "roof", "chimney", "ditch", "doghouse", "tree",
-                                 "fence", "paint", "painter", "gutter", "apartment"
-                                 "paving")))
+FILE_NAME = ("sentences.txt", "userforms.txt")
 
 
 """**************************** Read text file ******************************"""
-def read_sen_file():
-    l_sent = []
-    try:
-        with open(SEN_FILE[0]) as file:
-            l_sent = file.readlines()
-    except IOError as err:
-        print("I/O Error %s" %(err))
-        sys.exit()
-    else:
-        file.close()
-    return l_sent
+def read_file(name, mode=None):
+    if type(name) == str:
+        try:
+            with open(name) as file:
+                l_sent = file.readlines()
+        except IOError as err:
+            print("I/O Error %s" %(err))
+            sys.exit()
+        else:
+            file.close()
+        if mode == "sen":
+            return l_sent
+        elif mode == "form":
+            userform = []; tokens = []
+            for s in l_sent:
+                tokens = tokenize.word_tokenize(s)
+                userform.append([w for w in tokens if w.isalnum()])
+        return userform
 
 
 """**************** Convert word to num to represent hours  *****************"""
@@ -126,7 +91,7 @@ def get_synonyms(token):
 def spell_check(tokens):
     if type(tokens) == list:
         sd = enchant.Dict("en_US")
-        sd.add("!"); sd.add("$"); sd.add("?")
+        sd.add("!"); sd.add("$")
         length = len(tokens)
         new_tokens = [None]*length
         tokens_range = range(length)
@@ -134,7 +99,7 @@ def spell_check(tokens):
             suggestions = sd.suggest(tokens[i])
             if sd.check(tokens[i]):
                 new_tokens[i] = tokens[i]
-            elif suggestions and dist(tokens[i], suggestions[0]) < MAX_DIST[0]:
+            elif suggestions and dist(tokens[i], suggestions[0]) <= MAX_DIST[0]:
                 new_tokens[i] = suggestions[0]
             else: new_tokens[i] = tokens[i]
         return new_tokens
@@ -182,43 +147,42 @@ def get_digits(tags):
                     digits.append(word_to_num(t[0]))
         digits.sort()
     return digits
+
     
-
 """************************** Recognition process ***************************"""
-def recognition_process(args):
-    if type(args) == list and type(args[0]) == tuple:
-        threshold_max = index = int()
+def recognition_process(tags, form):
+    if type(tags) == list and type(tags[0]) == tuple and type(form) == list:
+        threshold_max = 0
+    
+        # merge extra words and synonyms in one list for every userform
+        for __list in form:
+            __list.append(get_synonyms(__list.__getitem__(0)))
 
-        # merge extra words and synonyms in one list, for every userform
-        words_dict = [get_synonyms(f[0]) for f in USER_FORM]
-        for i, form in enumerate(USER_FORM):
-            for extra_word in form[1]:
-                words_dict[i].append(extra_word)
-        
-        dict_range = [range(len(l)) for l in words_dict]
-        form_range = range(len(USER_FORM))
-        for tag in args:
+        # strategy: try to find the best match between 2 words (maximum treshold)    
+        for tag in tags:
             if tag[1] == "NOUN" or tag[1] == "ADJ" or tag[1] == "VERB":
-                for i in form_range:
-                    for j in dict_range[i]:
-                        tmp = get_threshold(tag[0], words_dict[i][j])
-                        if tmp >= THRESHOLD[0]:
+                for __list in form:
+                    for w in __list:
+                        tmp = get_threshold(tag[0], w)
+                        if tmp >= THRESHOLD[0] and threshold_max < tmp:
                             threshold_max = tmp
-                            index = i
+                            userform_name = __list.__getitem__(0)
                             if threshold_max == 1: break
         if threshold_max:
-            return USER_FORM[index][0]
+            return userform_name
         
                         
 """******************************* Entry Point ******************************"""
 if __name__ == "__main__":
+    # read the userforms from file
+    w_dict = read_file(FILE_NAME[1], mode="form")
     index = 0
     while True: 
         # clearing the screen
         print("\n" * 100)
         
         # read the sentences from file
-        l_sent = read_sen_file()
+        l_sent = read_file(FILE_NAME[0], mode="sen")
 
         # select a sentence
         index %= len(l_sent)
@@ -243,12 +207,13 @@ if __name__ == "__main__":
             tags = pos_tag(filt_tokens, tagset="universal")
         
             # relation recognition
-            user_form = recognition_process(tags)
+            user_form = recognition_process(tags, w_dict)
     
         print("\nSENTENCE #%d\n" %(index), sentence)
         print("\nVALID FORMAT:", format_is_valid)
         
         if format_is_valid:
+            pass
             print("\n\nTOKENS\n", tokens)
             print("\n\nSPELL CHECK\n", modif_tokens)
             print("\n\nFILTERED TOKENS\n", filt_tokens)
